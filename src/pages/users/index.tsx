fe6f369817e4d4a00f7a033c03d90d021bc03f3c
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-09 15:19:41 -0300

    feat: create pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,6 +4,7 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { Pagination } from '../../components/Pagination';
 
 export default function UserList() {
   return (
@@ -74,6 +75,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
