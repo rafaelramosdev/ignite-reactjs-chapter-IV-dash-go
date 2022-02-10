4ace3dd8a7934b086adecb7e66392671af442eef
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-10 15:20:42 -0300

    feat: separate 'Header', 'Pagination' and 'Sidebar' components into smaller components

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -2,6 +2,10 @@ import { Flex, Input, Icon } from "@chakra-ui/react"
 
 import { RiSearchLine } from 'react-icons/ri'
 
+// two ways to build forms in react
+// - controlled components (ex: const [search, setSearch] = useState(''))
+// - uncontrolled components (ex: const searchInputRef - useRef<HTMLInputElement>(null) -> searchInputRef.current.value // null because initially this ref will have no element inside it and from the moment the input is built into the screen, then the ref will receive the input within it
+
 export function Search() {
   return (
     <Flex 
