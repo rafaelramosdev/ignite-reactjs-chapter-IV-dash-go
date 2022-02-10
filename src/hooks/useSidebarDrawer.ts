d2404aa629ea287306184c504f03884bb93bb165
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-10 14:21:10 -0300

    feat: create useSidebarDrawer hook

---
+++ src/hooks/useSidebarDrawer.ts
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+
+import { SidebarDrawerContext } from "../contexts/SidebarDrawerContext";
+
+export function useSidebarDrawer() {
+  const context = useContext(SidebarDrawerContext)
+
+  return context;
+}
