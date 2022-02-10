2a51567a96727e7825c81599111026bd3660752c
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-10 14:59:57 -0300

    feat: create functionality to flag active link in the Sidebar component

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -2,7 +2,7 @@ import { ElementType } from "react";
 
 import { Icon, Link as ChakraLink, LinkProps as ChakraLinkProps, Text } from "@chakra-ui/react";
 
-import Link from 'next/link'
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType; // uses this type when passing the name/reference of a component (example: <Icon as={RiDashboardLine} />) and not the declaration of a component (example: <Icon as={<RiDashboardLine />} />)
@@ -12,13 +12,13 @@ interface NavLinkProps extends ChakraLinkProps {
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" alignItems="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   )
 }
